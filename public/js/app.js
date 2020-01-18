bcf76e031ce6b78054f5c31570b011eee25783f0
Richard Emate <richardemate@gmail.com>
2020-01-18 12:37:13 +0100

    finishing about page

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,15 @@ window.addEventListener('load', () => {
     //compile Handlebar Templates 
     const homeTemplate = Handlebars.compile($('#home-template').html());
 
-    const html = homeTemplate();
-    el.html(html);
+    //const html = homeTemplate();
+    //el.html(html);
 });
+
+const changeNavStyle = () => {
+    $( document ).ready( function() {
+        if (document.URL.indexOf('about') > -1) {
+            document.querySelector('#majorNav').classList.remove('navbar-dark');
+            document.querySelector('#majorNav').classList.add('navbar-light');
+        }
+    })
+}
