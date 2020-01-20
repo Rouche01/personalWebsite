e210aec9739620b42e7057d98a6e512738ab8463
Richard Emate <richardemate@gmail.com>
2020-01-20 05:18:20 +0100

    got my data from api using axios

--- server.js
+++ server.js
@@ -4,11 +4,35 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const { allPosts } = require('./lib/datocms-service')
+
 // set public folder as root
 app.use(express.static('public'));
 
 // Allow front-end access to node_modules folder
 app.use('/scripts', express.static(`${__dirname}/node_modules/`));
+
+// Express error handler
+const errorHandler = (err, req, res) => {
+    if(err.response) {
+        res.status(403).send({title: 'Server responded with an error', message: err.message });
+    } else if(err.request) {
+        res.status(503).send({title: 'Unable to communicate with server', message: err.message});
+    } else {
+        res.status(500).send({title: 'An unexpected error occured', message: err.message});
+    }
+};
+
+// Fetch Blog Content
+app.get('/api/blogposts', async (req, res) => {
+    try {
+        const data = await allPosts();
+        res.setHeader('Content-Type', 'application/json');
+        res.send(data);
+    } catch(error) {
+        errorHandler(error, req, res);
+    }
+});
 
 // Redirect All traffic to index.html
 app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
@@ -17,3 +41,13 @@ app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
 app.listen(port, () => {
     console.log('listening on %d', port);
 });
+
+/** 
+const test = async () => {
+    const data = await allPosts();
+    console.log(data);
+}
+
+test();
+*/
+
